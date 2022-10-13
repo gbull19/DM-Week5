1caaa8a13d09d63a7815a5bd1f25e1ef2d7a2573
gbull19 <gbull19@yahoo.com>
2022-10-13 08:01:20 -0600

    solution 1 to guessGifts

--- day_04/challenges/guessGifts.js
+++ day_04/challenges/guessGifts.js
@@ -9,8 +9,15 @@ const presents = [
     {size: "small", clatters: "yes", weight: "light"}
 ];
 
-const guessGifts = (obj1, obj2) => {
-
+const guessGifts = (arr1, arr2) => {
+    let guesses = [];
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr1[i].size == arr2[j].size && arr1[i].clatters == arr2[j].clatters && arr1[i].weight == arr2[j].weight) {
+                guesses.push(arr1[i].name);
+            }
+        }
+    } return guesses;
 }
 
-guessGifts(wishlist, presents); // must return ["Toy Car", "Mini Puzzle"]
+console.log(guessGifts(wishlist, presents)); // must return ["Toy Car", "Mini Puzzle"]
