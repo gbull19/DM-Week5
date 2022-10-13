5544a29d0b6ebb420394576a8e77c9e7fe55e9b7
gbull19 <gbull19@yahoo.com>
2022-10-13 07:45:10 -0600

    initial commit

---
+++ day_04/challenges/guessGifts.js
@@ -0,0 +1,16 @@
+const wishlist = [
+    {name: "Mini Puzzle", size: "small", clatters: "yes", weight: "light"},
+    {name: "Toy Car", size: "medium", clatters: "a bit", weight: "medium"},
+    {name: "Card Game", size: "small", clatters: "no", weight: "light"}
+];
+
+const presents = [
+    {size: "medium", clatters: "a bit", weight: "medium"},
+    {size: "small", clatters: "yes", weight: "light"}
+];
+
+const guessGifts = (obj1, obj2) => {
+
+}
+
+guessGifts(wishlist, presents); // must return ["Toy Car", "Mini Puzzle"]
